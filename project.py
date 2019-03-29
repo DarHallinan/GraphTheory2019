95cb5e6dde9ac5153f4f2ba1121708b60db41e09
DarHallinan <G00342845@gmit.ie>
2019-03-29 14:40:45 +0000

    added the expression matching function

--- project.py
+++ project.py
@@ -2,21 +2,16 @@
 # Graph Theory Project 2019
 
 class state:
-    label = None
-    edge1 = None
-    edge2 = None
+    label, edge1, edge2 = None, None, None
 
 class nfa:
-    intial = None
-    accept = None
+    intial, accept = None, None
 
     def __init__(self, initial, accept):
-      self.intial = initial
-      self.accept = accept
+      self.intial, self.accept = initial, accept
 
 def shunt(infix):
-    operators = {'*': 30, '.': 20, '|': 10}
-
+    operators = {'*': 30, '+': 30, '?': 30, '.': 20, '|': 10}
     postfix, stack = ' ', ' '
 
     for c in infix:
@@ -37,63 +32,74 @@ def shunt(infix):
                 stack = stack[:-1]
             stack += c
         else:
-            postfix += c
-    
+            postfix += c 
     # copied from above to run if stack is not empty
     while stack:
         postfix += stack[-1]
-        stack = stack[:-1]
-    
+        stack = stack[:-1]   
     return postfix
 
-print(shunt("(a.b)|(c*.d)")) # shunting yard algorithm test 
 
-def compile(pofix):
+def compile(postfix):
     nfaStack = []
 
-    for c in pofix:
+    for c in postfix:
         # join the initial and accept states together to create a loop for your characters
         if c == '*':
             nfa = nfaStack.pop()
-            initial = state()
-            accept = state()
+            initial, accept = state(), state()
 
-            initial.edge1 = nfa.intial
-            initial.edge2 = accept
-            nfa.accept.edge1 = nfa.intial
-            nfa.accept.edge2 = accept
+            initial.edge1, nfa.accept.edge1 = nfa.intial
+            initial.edge2, nfa.accept.edge2 = accept, accept
             nfaStack.append(nfa(initial, accept))
-
         # merge the two automata by linking 1's accept to 2's initial states
         elif c == '.': 
-            nfa2 = nfaStack.pop()
-            nfa1 = nfaStack.pop()
+            nfa2, nfa1 = nfaStack.pop(), nfaStack.pop()
             nfa1.accept.edge1 = nfa2.intial
-            nfaStack.append(nfa1.intial, nfa2.accept)
-        
+            nfaStack.append(nfa1.intial, nfa2.accept)       
         # create new initial and accept states and use them to link nfa1 and nfa2
         elif c == '|':
-            nfa2 = nfaStack.pop()
-            nfa1 = nfaStack.pop()
-            initial = state()
-            accept = state()
-
-            initial.edge1 = nfa1.intial
-            initial.edge2 = nfa2.intial
+            nfa2, nfa1 = nfaStack.pop(), nfaStack.pop()
+            initial, accept = state(), state()
+            initial.edge1, initial.edge2 = nfa1.intial, nfa2.intial
             # both old accept states now point to our new accept state
-            nfa1.accept.edge1 = accept
-            nfa2.accept.edge1 = accept
-            nfaStack.append(nfa(initial, accept))
-        
-        # creates new states and edges
-        # labels each edge with what the current non-special character is
+            nfa1.accept.edge1, nfa2.accept.edge1 = accept, accept
+            nfaStack.append(nfa(initial, accept))      
+        # creates new states and edges; labels each edge with what the current non-special character is
         else:
-            accept = state()
-            initial = state()
+            initial, accept = state(), state()
             initial.label = c
             initial.edge1 = accept
             # create instance of class nfa()
-            nfaStack.append(nfa(initial, accept))
-    
+            nfaStack.append(nfa(initial, accept))   
     # should only ever have one nfa in the stack
     return nfaStack.pop()
+
+def helper(state):
+    states = set()
+    states.add(state)
+    # if arrow is labelled as 'E', or empty set
+    if state.label is None:
+        if state.edge1 is not None:
+            states |= helper(state.edge1)
+        if state.edge2 is not None:
+            states |= helper(state.edge2)
+    return states
+
+def match(infix, string):
+    # shunt and compile the expression to prepare it for matching
+    postfix = shunt(infix)
+    nfa = compile(postfix)
+
+    currentS, nextS = set(), set()
+    # add initial state to the working set
+    currentS |= helper(nfa.intial)
+
+    for s in string:
+        for c in currentS:
+            if c.label == s:
+                nextS |= helper(c.edge1)
+        currentS = nextS
+        nextS = set()
+
+    return(nfa.accept in currentS)
